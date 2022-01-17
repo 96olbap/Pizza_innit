8d69076358a21808bc7f6ff1d4c6214179ace2ec
96olbap <alvinwanjala@gmail.com>
2022-01-18 02:05:34 +0300

    export array objects from main.js to scripts.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,69 +1,110 @@
 const pizzaType = [{
-            id: 1,
-            toppings: 'Bacon',
-            size: 'small',
-            rating: 5.3,
-            price: 810,
-            img: 'images/small.jpg',
-            quantity: 1
-        },
-        {
-            id: 2,
-            toppings: 'Pepperoni',
-            size: 'small',
-            rating: 4.7,
-            price: 880,
-            img: 'images/small.jpg',
-            quantity: 1
-        },
-        {
-            id: 3,
-            toppings: 'Onions',
-            size: 'small',
-            rating: 3.9,
-            price: 795,
-            img: 'images/small.jpg',
-            quantity: 1
-        },
-        {
-            id: 4,
-            toppings: 'Green peppers',
-            size: 'small',
-            rating: 4.9,
-            price: 895,
-            img: 'images/small.jpg',
-            quantity: 1
-        },
-        {
-            id: 5,
-            toppings: 'Bacon',
-            size: 'medium',
-            rating: 5.9,
-            price: 1200,
-            img: 'images/medium.jpg',
-            quantity: 1
-        } {
-            id: 6,
-            toppings: 'Pepperoni',
-            size: 'medium',
-            rating: 5.7,
-            price: 1370,
-            img: 'images/medium.jpg',
-            quantity: 1
-        } {
-            id: 7,
-            toppings: 'Onions',
-            size: 'medium',
-            rating: 5.5,
-            price: 1245,
-            img: 'images/medium.jpg',
-            quantity: 1
-        } {
-            id: 8,
-            toppings: 'Green peppers',
-            size: 'medium',
-            rating: 5.9,
-            price: 1400,
-            img: 'images/medium.jpg',
-            quantity: 1
-        },
+        id: 1,
+        toppings: 'Bacon',
+        size: 'small',
+        rating: 5.3,
+        price: 810,
+        img: 'images/small.jpg',
+        quantity: 1
+    },
+    {
+        id: 2,
+        toppings: 'Pepperoni',
+        size: 'small',
+        rating: 4.7,
+        price: 880,
+        img: 'images/small.jpg',
+        quantity: 1
+    },
+    {
+        id: 3,
+        toppings: 'Onions',
+        size: 'small',
+        rating: 3.9,
+        price: 795,
+        img: 'images/small.jpg',
+        quantity: 1
+    },
+    {
+        id: 4,
+        toppings: 'Green peppers',
+        size: 'small',
+        rating: 4.9,
+        price: 895,
+        img: 'images/small.jpg',
+        quantity: 1
+    },
+    {
+        id: 5,
+        toppings: 'Bacon',
+        size: 'medium',
+        rating: 5.9,
+        price: 1200,
+        img: 'images/medium.jpg',
+        quantity: 1
+    },
+    {
+        id: 6,
+        toppings: 'Pepperoni',
+        size: 'medium',
+        rating: 5.7,
+        price: 1370,
+        img: 'images/medium.jpg',
+        quantity: 1
+    },
+    {
+        id: 7,
+        toppings: 'Onions',
+        size: 'medium',
+        rating: 5.5,
+        price: 1245,
+        img: 'images/medium.jpg',
+        quantity: 1
+    },
+    {
+        id: 8,
+        toppings: 'Green peppers',
+        size: 'medium',
+        rating: 5.9,
+        price: 1400,
+        img: 'images/medium.jpg',
+        quantity: 1
+    },
+    {
+        id: 9,
+        toppings: 'Bacon',
+        size: 'large',
+        rating: 5.9,
+        price: 1470,
+        img: 'images/large.jpg',
+        quantity: 1
+    },
+    {
+        id: 10,
+        toppings: 'Pepperoni',
+        size: 'large',
+        rating: 5.7,
+        price: 1550,
+        img: 'images/large.jpg',
+        quantity: 1
+    },
+    {
+        id: 11,
+        toppings: 'Onions',
+        size: 'mlarge',
+        rating: 5.5,
+        price: 1540,
+        img: 'images/large.jpg',
+        quantity: 1
+    },
+    {
+        id: 12,
+        toppings: 'Green peppers',
+        size: 'large',
+        rating: 5.9,
+        price: 1660,
+        img: 'images/large.jpg',
+        quantity: 1
+    },
+]
+export { pizzaType };
