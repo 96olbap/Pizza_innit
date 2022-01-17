29a3e9171411046452756c728676d9a0555fab37
96olbap <alvinwanjala@gmail.com>
2022-01-18 02:48:18 +0300

    update scripts,js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,5 +12,29 @@ function showItems() {
     console.log(smallInfo);
     const largeInfo = pizzaType.filter(item => item.size == 'large')
     console.log(smallInfo);
+
+    smallInfo.map(item => {
+        var itenCard = document.createElement('div');
+        itenCard.setAttribute('id', 'item-card');
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+        var star = document.createElement('i');
+        star.setAttribute('id', 'rating');
+        star.setAttribute('class', 'fa fa-star')
+        star.innerText = '' + item.rating;
+        var heart = document.createElement('i');
+        star.setAttribute('id', 'rating');
+        heart.setAttribute('class', 'fa fa-heart-o add-to-cart')
+        heart.innerText = '' + item.rating;
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+
+
+
+    })
 }
-showItems()
+showItems();
