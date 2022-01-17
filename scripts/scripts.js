c88b34344cdd25c3ecccce94799f79b84816c679
96olbap <alvinwanjala@gmail.com>
2022-01-18 02:21:52 +0300

    Add function showitrms

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,16 @@
+import { pizzaType } from "./main.js";
+console.log(pizzaType);
+
 function showItems() {
     var small = document.getElementById('small');
     var medium = document.getElementById('large');
     var large = document.getElementById('Xlarge');
 
-    const smallInfo = foodItem.filter(item => item.category == 'small')
+    const smallInfo = pizzaType.filter(item => item.size == 'small')
+    console.log(smallInfo);
+    const mediumInfo = pizzaType.filter(item => item.size == 'medium')
+    console.log(smallInfo);
+    const largeInfo = pizzaType.filter(item => item.size == 'large')
     console.log(smallInfo);
 }
+showItems()
