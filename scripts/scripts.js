14e0f2877db621ddabd5338a0292210491a4f1c3
96olbap <alvinwanjala@gmail.com>
2022-01-18 07:26:53 +0300

    add select options to index file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -40,12 +40,12 @@ function showItems() {
         bei.setAttribute('id', 'pizza-price');
         bei.innerText = 'Cost: Kes.' + item.price;
 
-        itenCard.appendChild(cardTop);
-        itenCard.appendChild(img);
-        itenCard.appendChild(toppings);
-        itenCard.appendChild(bei);
+        // itenCard.appendChild(cardTop);
+        // itenCard.appendChild(img);
+        // itenCard.appendChild(toppings);
+        // itenCard.appendChild(bei);
 
-        small.appendChild(itenCard);
+        // small.appendChild(itenCard);
     });
     mediumInfo.map(item => {
         var itenCard = document.createElement('div');
@@ -74,12 +74,12 @@ function showItems() {
         bei.setAttribute('id', 'pizza-price');
         bei.innerText = 'Cost: Kes.' + item.price;
 
-        itenCard.appendChild(cardTop);
-        itenCard.appendChild(img);
-        itenCard.appendChild(toppings);
-        itenCard.appendChild(bei);
+        // itenCard.appendChild(cardTop);
+        // itenCard.appendChild(img);
+        // itenCard.appendChild(toppings);
+        // itenCard.appendChild(bei);
 
-        medium.appendChild(itenCard);
+        // medium.appendChild(itenCard);
     });
     largeInfo.map(item => {
         var itenCard = document.createElement('div');
@@ -108,12 +108,12 @@ function showItems() {
         bei.setAttribute('id', 'pizza-price');
         bei.innerText = 'Cost: Kes.' + item.price;
 
-        itenCard.appendChild(cardTop);
-        itenCard.appendChild(img);
-        itenCard.appendChild(toppings);
-        itenCard.appendChild(bei);
+        // itenCard.appendChild(cardTop);
+        // itenCard.appendChild(img);
+        // itenCard.appendChild(toppings);
+        // itenCard.appendChild(bei);
 
-        large.appendChild(itenCard);
+        // large.appendChild(itenCard);
     });
 }
 showItems();
@@ -121,7 +121,7 @@ showItems();
 const mediumInfo = [...new Map(pizzaType.map(item => [item['toppings'], item])).values()];
 console.log(mediumInfo);
 
-function pickTaste() {
+function pickFlavour() {
     var cartList = document.getElementById('cart-list');
 
     mediumInfo.map(item => {
@@ -141,9 +141,83 @@ function pickTaste() {
         listCard.appendChild(listName);
 
         var cloneListCard = listCard.cloneNode(true);
-        categoryList.appendChild(listCard);
-        document.querySelector('.sec-name').appendChild(cloneListCard)
+        cartList.appendChild(listCard);
+        document.querySelector('.sec-name').appendChild(cloneListCard);
     })
     console.log('pablo');
 }
-pickTaste();
+pickFlavour();
+
+document.querySelectorAll('.add-to-cart').forEach(item => {
+    item.addEventListener('click', addToCart)
+})
+
+var cartInfo = [];
+
+function addToCart() {
+    var itemToAdd = this.parentNode.nextSibling.nextSibling.innerText;
+    var itemObj = pizzaType.find(element => element.name == itemToAdd);
+
+    var index = cartInfo.indexOf(itemObj);
+    if (index === -1) {
+        document.getElementById(itemObj.id).classList.add('toggle-heart');
+        cartData = [...cartData, itemObj];
+    } else if (index > -1) {
+        alert("Added to cart!");
+    }
+
+    document.getElementById('cart-plus').innerText =
+        ' ' + cartData.length + ' Items';
+    document.getElementById('m-cart-plus').innerText =
+        ' ' + cartData.length;
+    totalAmount();
+    cartItems();
+}
+
+function cartItems() {
+    var tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = '';
+
+    cartInfo.map(item => {
+        var tableRow = document.createElement('tr');
+
+        var rowInfo1 = document.createElement('td');
+        var img = document.createElement('img');
+        img.src = item.img;
+        rowInfo1.appendChild(img);
+
+        var rowInfo2 = document.createElement('td');
+        rowInfo2.innerText = item.toppings;
+
+        var rowInfo3 = document.createElement('td');
+        var btn1 = document.createElement('button');
+        btn1.setAttribute('class', 'reduce-items');
+        btn1.innerText = '-';
+        var span = document.createElement('span');
+        span.innerText = item.quantity;
+        var btn2 = document.createElement('button');
+        btn2.setAttribute('class', 'increase-items');
+        btn2.innerText = '+';
+
+        rowInfo3.appendChild(btn1);
+        rowInfo3.appendChild(span);
+        rowInfo3.appendChild(btn2);
+
+        var rowInfo4 = document.createElement('td');
+        rowInfo4.innerText = item.price;
+
+        tableRow.appendChild(rowInfo1);
+        tableRow.appendChild(rowInfo2);
+        tableRow.appendChild(rowInfo3);
+        tableRow.appendChild(rowInfo4);
+
+        tableBody.appendChild(tableRow);
+    });
+    document.querySelectorAll('.increase-items').forEach(item => {
+        item.addEventListener('click', incrementItem)
+    });
+
+    document.querySelectorAll('.reduce-item').forEach(item => {
+        item.addEventListener('click', decrementItem);
+    });
+}
